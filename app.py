ff2bd6dc5f1c7202c3b30f7a4f97395dbcf9e782
Adriana Luksová <120632555+AdrianaLuksova@users.noreply.github.com>
2024-11-18 08:45:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return 'TOTO JE MUJ WEB SERVER'
 
+@app.route('/develop')
+def test_stranka():
+    return 'test stranka'
+
 if __name__ == '__main__':
     app.run(debug=True)
